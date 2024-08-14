8d3e23dd66c4ce9ddd9cb9b7b2f0708026403912
Eze <eze.gonzalez@vera.com.uy>
2024-08-14 13:15:27 -0300

    👍

---
+++ main.js
@@ -0,0 +1,9 @@
+function repeatString(texto, repeticiones) {
+    for (i = 0; i < repeticiones; ++i) {
+        console.log(texto);
+    }
+}
+
+const texto = "tirabuzones con tuco";
+const veces = 10;
+repeatString(texto, veces);
